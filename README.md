7442814aa31a0a644db6d7c49059a7144f3cd627
ferutizi <fer.utizi@gmail.com>
2023-08-27 14:28:46 -0300

    Print questions, select range, and results

---
+++ README.md
@@ -0,0 +1,14 @@
+Desafío: Desarrollo de una encuesta en React con preguntas y valoraciones
+
+Descripción: Debes crear una aplicación web que permita a los usuarios responder una encuesta con preguntas y valoraciones del 1 al 5. El listado de preguntas será cargado desde un JSON mockeado. La aplicación debe cumplir con los siguientes requisitos:
+
+Mostrar una pregunta a la vez, con sus opciones de valoración del 1 al 5.
+Permitir al usuario seleccionar una valoración para cada pregunta.
+Mostrar la siguiente pregunta después de que el usuario haya respondido la actual.
+Al finalizar la encuesta, mostrar un resumen con las respuestas del usuario.
+Requisitos técnicos:
+Utiliza React para construir la aplicación.
+Para el CSS utilizar algún framework como Bootstrap o Material-UI.
+Entrega: Una vez completado, puedes compartir tu solución proporcionando el código fuente en un repositorio de GitHub u otra plataforma de alojamiento de código. Asegúrate de incluir instrucciones claras sobre cómo ejecutar y probar la aplicación.
+
+¡Buena suerte en el desafío!
--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,50 @@
 import './App.css'
+import { useState } from 'react'
+import questions from '../Questions.json'
 
+interface Question {
+	id: number,
+	texto: string
+}
+
+interface Answer extends Question {
+	valoracion: 1 | 2 | 3 | 4 | 5
+}
 function App(): JSX.Element {
+	const [answers, setAnswers] = useState<Answer[]>([])
+	const CurrentQuestion = questions.preguntas[answers.length]
+
+	const handleRate = (e: React.ChangeEvent<HTMLSelectElement>) => {
+		setAnswers(answers => answers.concat({
+			...CurrentQuestion,
+			valoracion: Number(e.target.value) as Answer['valoracion']
+		}))
+	}
+
+	if (!CurrentQuestion) {
+		return(
+			<ul>
+				{answers.map(item => (
+					<li key={item.id}>
+						{item.texto} - {item.valoracion}
+					</li>
+				))}
+			</ul>
+		)
+	}
+
 	return (
 		<>
-			<h1>Holi</h1>
+			<h1>Holi ☆★</h1>
+			<p>{CurrentQuestion.texto}</p>
+			<select onChange={handleRate}>
+				<option value='1'>1</option>
+				<option value='2'>2</option>
+				<option value='3'>3</option>
+				<option value='4'>4</option>
+				<option value='5'>5</option>
+			</select>
+			{/* 			<span>{'★'.repeat(3).padEnd(5, '☆')}</span>	 */}
 		</>
 	)
 }
